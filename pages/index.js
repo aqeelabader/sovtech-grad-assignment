1ed5b216e2f00d0cc033d1458773c38df8cbe4eb
Aqeel <abader.aqeel@gmail.com>
2022-06-19 23:56:19 +0200

    top

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,18 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Aqeel SovTech Assignment</title>
-        <meta name="description" content="coming soon" />
+        <title>Aqeel - SovTech</title>
+        <meta name="description" content="Assignment for SovTech grad program" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          coming soon...nice things being developed.
-        </h1>
+
+      <section>
+          <div>
+          <h1>Aqeel Abader - SovTech Assignment</h1>
+          </div>
+        </section>
 
       </main>
        
